0ad5fcffe05fa98742ec9619593275c44a2452e2
MinKhant610 <minkhantiopp@gmail.com>
2024-08-04 12:42:43 +0630

    day 6

--- projects/lottery.py
+++ projects/lottery.py
@@ -6,26 +6,59 @@ ran_num2 = randint(0, 9)
 lottery_num = str(ran_num1)+str(ran_num2) # change type for checking 
 
 lottery_list = [] 
+unit = 0 # user add amount 
+user_amount = 0
+total_bet = 0
 
-total_lot = int(input('Enter total number of lottery :'))
 
-#check total lottery list
-while total_lot != 0:
-    money = int(input('Enter the money want to bet :'))
-    usr_num = str(input('Enter the lottery num :'))
-    check_r = str(input('R ? :'))
+#for money control 
+print('\n------------------------------------------')
+print('      -----Welcome to lottery bet-----        ')
+print('------------------------------------------')
+print('You can add at least 1000 or more')
+unit = int(input('Enter the money want to add :'))
 
-    lottery_list.append(usr_num)
-    # check reverse or not 
-    if check_r.upper() == 'R' :
-        reverse_num = usr_num[::-1]
-        lottery_list.append(reverse_num)
-    else :
-        reverse_num = 0
+if unit < 1000:
+    unit = 0
+    print('Add at least one thousand')
+else :
+    user_amount += unit 
+    print('Now your balance is :', unit)
+    total_lot = int(input('Enter total number of lottery :'))
 
-    total_lot -= 1
+    #check total lottery list
+    while total_lot != 0:
+        # if user_amount == 0:
+        #     print('Not enough money')
+        #     break
 
-#show random lottery and check win or lose 
+        print('\n-----------------------------------------')
+        money = int(input('Enter the money want to bet :'))
+        usr_num = str(input('Enter the lottery num :'))
+        check_r = str(input('R ? :'))
+
+        lottery_list.append(usr_num)
+
+        # check reverse or not 
+        if check_r.upper() == 'R' :
+            r_money = int(input('Enter amount of R money :')) 
+            money += r_money
+            reverse_num = usr_num[::-1]
+            lottery_list.append(reverse_num)
+        else :
+            reverse_num = 0
+
+        user_amount -= money # reduce user money 
+        total_bet += money
+        total_lot -= 1 #number of lottery 
+
+        # show total bet money 
+        print('\n-----------------------------------------')
+        print (f'Total bet {total_bet} units ')
+        print ('Now your balance is :', user_amount)
+
+
+# show random lottery and check win or lose 
 print('\nYour lottery num are : ', ' '.join(lottery_list))
 print("\n---------------------------------------")
 print(f"Today lottery number is {ran_num1}{ran_num2}") 
